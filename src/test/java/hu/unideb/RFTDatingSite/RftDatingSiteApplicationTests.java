e81a667947c0b64f79ef6358cdd8ab9a203679b2
Czeglédi Attila <czegiati@gmail.com>
2019-10-29 16:43:14 +0100

    Vaidations on the registery form and custom annotation added.

---
+++ src/test/java/hu/unideb/RFTDatingSite/RftDatingSiteApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.unideb.RFTDatingSite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RftDatingSiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
